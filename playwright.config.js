4e4c0ea9d301418e12212c6116eb395eceba7b7b
supaizjy0321 <166015407+supaizjy0321@users.noreply.github.com>
2025-04-14 04:12:45 +0300

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,20 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests/e2e',
+  timeout: 30 * 1000,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL || 'https://witty-water-006f88503.6.azurestaticapps.net',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
